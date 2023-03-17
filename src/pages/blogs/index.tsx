bda15d51f262fc807c6c1548978588ec822849ec
yechuan51 <96866332+yechuan51@users.noreply.github.com>
2023-03-17 17:22:15 +0800

    Make time display rendered on server side.

---
+++ src/pages/blogs/index.tsx
@@ -0,0 +1,7 @@
+export default function BlogsPage() {
+  return (
+    <div className="flex justify-center flex-col items-center">
+      <h1 className="text-2xl mt-4">Blogs</h1>
+    </div>
+  );
+}
