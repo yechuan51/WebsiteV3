34e1cbad3f974a15618fb92318a2324ef16aa677
yechuan51 <96866332+yechuan51@users.noreply.github.com>
2023-03-17 13:59:31 +0800

    Adding a date time display to home page.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,9 @@
+import CurrentTimeComp from "@/components/navBar/client/utils/currentTime";
+
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <div className="flex justify-end items-start top-0 right-0">
+      <CurrentTimeComp />
+    </div>
+  );
 }
