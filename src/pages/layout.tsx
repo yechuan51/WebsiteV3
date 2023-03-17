1de7f348700ebdcc8c0d1bad8b4059dbee3a0697
yechuan51 <96866332+yechuan51@users.noreply.github.com>
2023-03-17 13:44:22 +0800

    Commit before creating repo.

---
+++ src/pages/layout.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div>
+      <Head>
+        <title>My Website</title>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <meta name="description" content="My Website" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <body>{children}</body>
+    </div>
+  );
+}
